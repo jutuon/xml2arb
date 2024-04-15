900df53ed8db9605776dc61d4ce34aa988bf77c4
Juuso Tuononen <jutuon@outlook.com>
2024-04-15 17:24:20 +0300

    Error handling improvements
    Make error messages more nice and exit process with exit code 1 if error
    occurs.

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,16 @@ pub mod xml;
 fn main() {
     let config = config::get_config();
 
-    let parsed_strings = xml::parse_android_strings_xml_files(&config.input_dir).unwrap();
-    arb::save_to_arb_files(&config.output_dir, parsed_strings, config.arb_file_name_template).unwrap();
+    let parsed_strings = match xml::parse_android_strings_xml_files(&config.input_dir) {
+        Ok(parsed_strings) => parsed_strings,
+        Err(e) => {
+            eprintln!("Error: {}", e);
+            std::process::exit(1);
+        }
+    };
+
+    if let Err(e) = arb::save_to_arb_files(&config.output_dir, parsed_strings, config.arb_file_name_template) {
+        eprintln!("Error: {}", e);
+        std::process::exit(1);
+    }
 }
